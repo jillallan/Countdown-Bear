e311886b368797f88c021c96bc5e4db37d0bc049
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-25 21:30:54 +0100

    Add new test target

---
+++ Countdown Bear NewUITests2/Countdown_Bear_NewUITests2LaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Countdown_Bear_NewUITests2LaunchTests.swift
+//  Countdown Bear NewUITests2
+//
+//  Created by Jill Allan on 25/10/2023.
+//
+
+import XCTest
+
+final class Countdown_Bear_NewUITests2LaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
